c346ceeb2f485c7827e20afac523ca5bcf7ec8eb
Thaidao <daonamthai@gmail.com>
2020-04-23 17:56:02 +0700

    feat: add inintial project

---
+++ MFCApplication5/MFCApplication5Dlg.cpp
@@ -0,0 +1,121 @@
+
+// MFCApplication5Dlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "MFCApplication5.h"
+#include "MFCApplication5Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication5Dlg dialog
+
+
+
+CMFCApplication5Dlg::CMFCApplication5Dlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CMFCApplication5Dlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication5Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication5Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CMFCApplication5Dlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication5Dlg message handlers
+
+BOOL CMFCApplication5Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CMFCApplication5Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CMFCApplication5Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+int DisplayResourceNAMessageBox()
+{
+	int msgboxID = MessageBox(
+		NULL,
+		(LPCWSTR)L"Resource not available\nDo you want to try again?",
+		(LPCWSTR)L"Account Details",
+		MB_ICONWARNING | MB_CANCELTRYCONTINUE | MB_DEFBUTTON2
+		);
+
+	switch (msgboxID)
+	{
+		case IDCANCEL:
+			// TODO: add code
+			break;
+		case IDTRYAGAIN:
+			// TODO: add code
+			break;
+		case IDCONTINUE:
+			// TODO: add code
+			break;
+	}
+
+	return msgboxID;
+}
+
+
+void CMFCApplication5Dlg::OnBnClickedOk()
+{
+	// TODO: Add your control notification handler code here
+	CDialogEx::OnOK();
+
+	DisplayResourceNAMessageBox();
+}
---
+++ MFCApplication5/MFCApplication5Dlg.h
@@ -0,0 +1,33 @@
+
+// MFCApplication5Dlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCApplication5Dlg dialog
+class CMFCApplication5Dlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCApplication5Dlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MFCAPPLICATION5_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
